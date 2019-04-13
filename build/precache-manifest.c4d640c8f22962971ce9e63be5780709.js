0ebabef81d1cfa9e58acc3d9505ca39c2d88fa97
wabens <waltkbenson@gmail.com>
2019-04-13 09:39:27 -0500

    form is in grid, inputs don't go anywhere

---
+++ build/precache-manifest.c4d640c8f22962971ce9e63be5780709.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3b33f70008357260bb0",
+    "url": "/static/css/main.001b3e9e.chunk.css"
+  },
+  {
+    "revision": "f3b33f70008357260bb0",
+    "url": "/static/js/main.f3b33f70.chunk.js"
+  },
+  {
+    "revision": "b4415ee687ffadf98368",
+    "url": "/static/js/1.b4415ee6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4d7430efff55fb5cf2aa6aaa1b94989a",
+    "url": "/index.html"
+  }
+];
--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
+
+
 
 
 
@@ -20,69 +29,110 @@ class Admin extends Component {
     }
 
     handleChange = (event) => {
-        console.log(`handleChange `, event.target.value);
+        //console.log(`handleChange `, event.target.value);
         this.setState({
             project:{
                 [event.target.name]: event.target.value
             }
         })
-        
-        
     }
 
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`submit`);
+        
+    }
+        
+        
+
+
     render() {
+        console.log(`state `, this.state.project);
+        
         return (
         <section>
             <form>
-                <TextField
-                    label="Name"
-                    name = 'name'
-                    value={this.state.project.name}
-                    onChange={this.handleChange}
-                    margin="normal"
-                    variant = "outlined"
+            <Grid spacing={8} container>
+                <Grid item>
+                    <TextField
+                        label="Name"
+                        name = 'name'
+                        value={this.state.project.name}
+                        onChange={this.handleChange}
+                        margin="normal"
+                        variant = "outlined"
+                    />
+                </Grid>
+                <Grid item>
+                    <TextField
+                        label="Description"
+                        name = 'description'
+                        value={this.state.project.description}
+                        onChange={this.handleChange}
+                        margin="normal"
+                        variant="outlined"
+                    />
+                </Grid>
+                <Grid item>
+                    <TextField
+                        label="Thumbnail"
+                        value={this.state.project.thumbnail}
+                        name = 'thumbnail'
+                        onChange={this.handleChange}
+                        margin="normal"
+                        variant="outlined"
+                    />
+                </Grid>
+                <Grid item>
+                    <TextField
+                        label="Website"
+                        value={this.state.project.website}
+                        name = 'website'
+                        onChange={this.handleChange}
+                        margin="normal"
+                        variant="outlined"
+                    />
+                </Grid>
+                <Grid item>
+                    <TextField
+                        label="Github"
+                        value={this.state.project.github}
+                        name = 'github'
+                        onChange={this.handleChange}
+                        margin="normal"
+                        variant="outlined"
+                    />
+                </Grid>
+                <Grid item>
+                    <TextField
+                        label="Date"
+                        value={this.state.project.date_completed}
+                        name = 'date_completed'
+                        onChange={this.handleChange}
+                        margin="normal"
+                        variant="outlined"
+                    />
+                </Grid>
+                <Grid item>
+                    <FormControl>
+                        <InputLabel>Tag</InputLabel>
+                        <NativeSelect
+                            value={this.state.project.tag_id}
+                            onChange={this.handleChange}
+                            input={<Input name="tag_id" id="age-native-helper" />}
+                        >
+                            <option value="" />
+                            <option value={1}>one</option>
+                            <option value={2}>two</option>
+                            <option value={3}>three</option>
+                        </NativeSelect>
+                    </FormControl>
+                </Grid>
+                <Grid item>
+                    <Button onClick={this.handleSubmit}>Submit</Button>
+                </Grid>
 
-                />
-                <TextField
-                    label="Description"
-                    name = 'description'
-                    value={this.state.project.description}
-                    onChange={this.handleChange}
-                    margin="normal"
-                    variant="outlined"
-                />
-                <TextField
-                    label="Thumbnail"
-                    value={this.state.project.thumbnail}
-                    name = 'thumbnail'
-                    onChange={this.handleChange}
-                    margin="normal"
-                    variant="outlined"
-                />
-                <TextField
-                    label="Website"
-                    value={this.state.project.website}
-                    name = 'website'
-                    onChange={this.handleChange}
-                    margin="normal"
-                    variant="outlined"
-                />
-                <TextField
-                    label="Github"
-                    value={this.state.project.github}
-                    name = 'github'
-                    onChange={this.handleChange}
-                    margin="normal"
-                    variant="outlined"
-                />
-                <TextField
-                    label="Date"
-                    value={this.state.project.date_completed}
-                    name = 'date_completed'
-                    onChange={this.handleChange}
-                    margin="normal"
-                    variant="outlined"
-                />
+            </Grid>
             </form>
         </section>
         );
